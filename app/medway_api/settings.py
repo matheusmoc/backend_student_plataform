29943cc4661833ecda9646d77703d2b13d70469c
matheusmoc <matheusandrade009@gmail.com>
2025-11-13 16:49:37 -0300

    Update database settings to support Docker and fallback to SQLite

--- app/medway_api/settings.py
+++ app/medway_api/settings.py
@@ -79,16 +79,25 @@ WSGI_APPLICATION = "medway_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        'ENGINE': "django.db.backends.postgresql",
-        'NAME': os.environ.get("POSTGRES_DB", "teste"),
-        'USER': os.environ.get("POSTGRES_USER"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
-        'PORT': os.environ.get("POSTGRES_PORT", "5432"),
+# Database
+if os.environ.get('USE_DOCKER'):
+    DATABASES = {
+        "default": {
+            'ENGINE': "django.db.backends.postgresql",
+            'NAME': os.environ.get("POSTGRES_DB", "teste"),
+            'USER': os.environ.get("POSTGRES_USER"),
+            'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+            'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+            'PORT': os.environ.get("POSTGRES_PORT", "5432"),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

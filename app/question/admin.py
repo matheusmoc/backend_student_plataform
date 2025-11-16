97787b5dd6249a64338fa4998428d5299f446a6a
matheusmoc <matheusandrade009@gmail.com>
2025-11-16 15:25:53 -0300

    Implement validation for single-choice questions to ensure only one correct alternative is allowed

--- app/question/admin.py
+++ app/question/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
+from django.forms.models import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 
 from question.models import Question, Alternative
+
+
+from question.utils import QuestiosTypeChoices
+
+
+class AlternativeInlineFormSet(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        if not hasattr(self, 'instance') or not self.instance:
+            return
+        if self.instance.selection_type == QuestiosTypeChoices.SINGLE:
+            correct_count = 0
+            for form in self.forms:
+                if not hasattr(form, 'cleaned_data'):
+                    continue
+                data = form.cleaned_data
+                if data.get('DELETE'):
+                    continue
+                if data.get('is_correct'):
+                    correct_count += 1
+            if correct_count > 1:
+                raise ValidationError('Questões de escolha única só podem ter uma alternativa correta.')
 
 
 class AlternativeInline(admin.TabularInline):
@@ -8,6 +32,7 @@ class AlternativeInline(admin.TabularInline):
     min_num = 1
     max_num = 5
     ordering = ('option',)
+    formset = AlternativeInlineFormSet
 
 
 @admin.register(Question)
--- app/question/models.py
+++ app/question/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction
 from django.core.exceptions import ValidationError
 
 from question.utils import AlternativesChoices, QuestiosTypeChoices
@@ -32,19 +32,17 @@ class Alternative(models.Model):
     def __str__(self):
         return f'{self.question_id} - {self.get_option_display()}: {self.content[:30]}'
 
-    def clean(self):
-        if self.is_correct and self.question_id:
-            try:
-                question = self.question  
-                if question.selection_type == QuestiosTypeChoices.SINGLE:
-                    qs = Alternative.objects.filter(question_id=self.question_id, is_correct=True)
-                    if self.pk:
-                        qs = qs.exclude(pk=self.pk)
-                    if qs.exists():
-                        raise ValidationError('Questões de escolha única só podem ter uma alternativa correta.')
-            except Question.DoesNotExist:
-                pass
-
     def save(self, *args, **kwargs):
+        if self.is_correct and self.question_id:
+
+            question = self.question
+            if question.selection_type == QuestiosTypeChoices.SINGLE:
+                with transaction.atomic():
+                    Alternative.objects.filter(
+                        question_id=self.question_id,
+                        is_correct=True
+                    ).exclude(pk=self.pk).update(is_correct=False)
+
+
         self.full_clean()
         return super().save(*args, **kwargs)
